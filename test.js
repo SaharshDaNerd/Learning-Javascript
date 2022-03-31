c0fd34bfa608789584996f04409a8e303d7c903a
SaharshDaNerd <saharshcodes@gmail.com>
2022-03-31 21:53:59 +0530

    changed a few things

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var firstName = "Saharsh";
-var lastName = "Saxena";
-var fullName = firstName + " " + lastName;
-console.log(fullName);
+//Take input from user and calculate the area of the circle
+const radius = prompt("Enter the radius of the circle");
+area = 3.14 * radius * radius;
+alert("The area of the circle is " + area);
